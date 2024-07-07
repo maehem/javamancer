f11419d32a20367fca7b5f51ddfb34235d0db00a
Mark J Koch <maehem@users.noreply.github.com>
2024-07-07 16:52:19 -0700

    Add DAT related files.

---
+++ src/main/java/com/maehem/javamancer/resource/view/DATUtil.java
@@ -0,0 +1,105 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.resource.view;
+
+import com.maehem.javamancer.logging.Logging;
+import com.maehem.javamancer.resource.file.PNGWriter;
+import com.maehem.javamancer.resource.model.DAT;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class DATUtil {
+
+    private static Logger LOG = Logging.LOGGER;
+
+    public static void createCache(DAT dat, File cacheFolder) {
+
+        File anhDir = new File(cacheFolder, "anh");
+        if (anhDir.isDirectory()) {
+
+        } else {
+            LOG.log(Level.SEVERE, "Cache 'anh' folder is not a folder!");
+        }
+
+        File bihDir = new File(cacheFolder, "bih");
+        if (bihDir.isDirectory()) {
+
+        } else {
+            LOG.log(Level.SEVERE, "Cache 'bih' folder is not a folder!");
+        }
+
+        File imhDir = new File(cacheFolder, "imh");
+        if (!imhDir.isDirectory()) {
+            if (imhDir.exists()) {
+                LOG.log(Level.SEVERE, "Cache 'imh' folder is not a folder. Deleting.");
+                imhDir.delete();
+            }
+            LOG.log(Level.SEVERE, "Cache 'imh' folder created.");
+            imhDir.mkdir();
+
+            populateIMH(dat, imhDir);
+        } else {
+            LOG.log(Level.SEVERE, "Cache 'imh' folder found.");
+        }
+
+        File picDir = new File(cacheFolder, "pic");
+        if (picDir.isDirectory()) {
+
+        } else {
+            LOG.log(Level.SEVERE, "Cache 'pic' folder is not a folder!");
+        }
+    }
+
+    private static void populateIMH(DAT dat, File folder) {
+        dat.imh.forEach((imh) -> {
+            LOG.log(Level.SEVERE, "Process IMH: " + imh.name);
+            PNGWriter pngWriter = new PNGWriter();
+
+            imh.dataBlock.forEach((blob) -> {
+                // Create Image
+                Image img = new Data2Image(blob, 0);
+                int indexOf = imh.dataBlock.indexOf(blob);
+                LOG.log(Level.SEVERE, "    Found sub-image: {0}x{1}", new Object[]{img.getWidth(), img.getHeight()});
+
+                try {
+                    // Save to PNG
+                    pngWriter.write(new File(folder, imh.name + "_" + (indexOf + 1) + ".png"), img);
+                } catch (IOException ex) {
+                    LOG.log(Level.SEVERE, null, ex);
+                }
+            });
+
+        });
+    }
+}
---
+++ src/main/java/com/maehem/javamancer/resource/view/Data2Image.java
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.resource.view;
+
+import com.maehem.javamancer.resource.file.Util;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class Data2Image extends WritableImage {
+
+    public Data2Image(byte[] data, int dataIndex) {
+        super(probeWidth(data, dataIndex), probeHeight(data, dataIndex));
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
+        }
+
+        dataIndex += 8; // X/Y W/H header is 8 bytes.
+        PixelWriter pw = getPixelWriter();
+        for (int row = 0; row < getHeight(); row++) {
+            for (int col = 0; col < getWidth(); col += 2) {
+                byte pixels = data[dataIndex];
+                dataIndex++;
+
+                // pixels contains two 4-bit indexed color nibbles.
+                pw.setColor(col, row, Color.web(Util.palette[(pixels & 0xF0) >> 4], 1.0));
+                pw.setColor(col + 1, row, Color.web(Util.palette[(pixels & 0xF)], 1.0));
+            }
+        }
+    }
+
+    private static int probeWidth(byte[] src, int srcIdx) {
+        return ((src[srcIdx + 5] & 0xFF) << 8) + ((src[srcIdx + 4] & 0xFF) * 2);
+    }
+
+    private static int probeHeight(byte[] src, int srcIdx) {
+        return ((src[srcIdx + 7] & 0xFF) << 8) + ((src[srcIdx + 6] & 0xFF));
+    }
+
+}
