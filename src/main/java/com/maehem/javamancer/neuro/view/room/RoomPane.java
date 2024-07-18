bc5ea8595a768ea10cd913e524b318b183b9b190
Mark J Koch <maehem@users.noreply.github.com>
2024-07-18 11:16:03 -0700

    Add bounds and player graphics.

--- src/main/java/com/maehem/javamancer/neuro/view/room/RoomPane.java
+++ src/main/java/com/maehem/javamancer/neuro/view/room/RoomPane.java
@@ -26,10 +26,15 @@
  */
 package com.maehem.javamancer.neuro.view.room;
 
+import com.maehem.javamancer.logging.Logging;
 import com.maehem.javamancer.neuro.model.Room;
+import com.maehem.javamancer.neuro.model.RoomBounds;
 import com.maehem.javamancer.neuro.view.ResourceManager;
+import java.util.logging.Logger;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  *
@@ -37,12 +42,104 @@ import javafx.scene.layout.Pane;
  */
 public class RoomPane extends Pane {
 
+    public static final Logger LOGGER = Logging.LOGGER;
+
+    private static final int DOOR_THICK = 12;
+    private final Rectangle boundsRect;
+    private final Rectangle topDoor;
+    private final Rectangle rightDoor;
+    private final Rectangle bottomDoor;
+    private final Rectangle leftDoor;
+
+    private final Room room;
+
     public RoomPane(ResourceManager resourceManager, Room room) {
+        this.room = room;
         ImageView roomView = new ImageView(resourceManager.getBackdrop(room));
         setLayoutX(15.5);
         setLayoutY(19);
 
-        getChildren().add(roomView);
+        RoomBounds bounds = RoomBounds.get(room);
+        boundsRect = initBoundsRect(bounds);
+        topDoor = initDoorTop(bounds);
+        rightDoor = initDoorRight(bounds);
+        bottomDoor = initDoorBottom(bounds);
+        leftDoor = initDoorLeft(bounds);
+
+        getChildren().addAll(roomView, boundsRect);
+        if (topDoor != null) {
+            getChildren().add(topDoor);
+        }
+        if (rightDoor != null) {
+            getChildren().add(rightDoor);
+        }
+        if (bottomDoor != null) {
+            getChildren().add(bottomDoor);
+        }
+        if (leftDoor != null) {
+            getChildren().add(leftDoor);
+        }
+
+        PlayerNode player = new PlayerNode(resourceManager);
+        player.setLayoutX(300);
+        player.setLayoutY(216);
+
+        getChildren().add(player);
+
     }
 
+    private Rectangle initBoundsRect(RoomBounds rb) {
+        Rectangle r = new Rectangle(
+                rb.lBound, rb.tBound,
+                rb.rBound - rb.lBound, rb.bBound - rb.tBound);
+        r.setFill(null);
+        r.setStroke(Color.GREEN);
+        r.setStrokeWidth(4);
+
+        return r;
+    }
+
+    private Rectangle initDoorTop(RoomBounds rb) {
+        if (rb.tx != 0 && rb.tw != 0) {
+            Rectangle r = new Rectangle(rb.tx, rb.tBound, rb.tw, DOOR_THICK);
+            r.setFill(Color.RED);
+            r.setStroke(null);
+            return r;
+        }
+
+        return null;
+    }
+
+    private Rectangle initDoorRight(RoomBounds rb) {
+        if (rb.ry != 0 && rb.rw != 0) {
+            Rectangle r = new Rectangle(rb.rBound, rb.ry, DOOR_THICK, rb.rw);
+            r.setFill(Color.RED);
+            r.setStroke(null);
+            return r;
+        }
+
+        return null;
+    }
+
+    private Rectangle initDoorBottom(RoomBounds rb) {
+        if (rb.bx != 0 && rb.bw != 0) {
+            Rectangle r = new Rectangle(rb.bx, rb.bBound - DOOR_THICK / 2, rb.bw, DOOR_THICK);
+            r.setFill(Color.RED);
+            r.setStroke(null);
+            return r;
+        }
+
+        return null;
+    }
+
+    private Rectangle initDoorLeft(RoomBounds rb) {
+        if (rb.ly != 0 && rb.lw != 0) {
+            Rectangle r = new Rectangle(rb.tBound, rb.ly, DOOR_THICK, rb.lw);
+            r.setFill(Color.RED);
+            r.setStroke(null);
+            return r;
+        }
+
+        return null;
+    }
 }
