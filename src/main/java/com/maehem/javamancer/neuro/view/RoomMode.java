ed67a0a45908f6b6158a8447a4d2416a9212584c
Mark J Koch <maehem@users.noreply.github.com>
2024-07-18 13:21:18 -0700

    Moused click handling.

--- src/main/java/com/maehem/javamancer/neuro/view/RoomMode.java
+++ src/main/java/com/maehem/javamancer/neuro/view/RoomMode.java
@@ -28,6 +28,7 @@ package com.maehem.javamancer.neuro.view;
 
 import com.maehem.javamancer.neuro.model.Room;
 import com.maehem.javamancer.neuro.view.room.RoomPane;
+import java.util.logging.Level;
 import javafx.scene.image.ImageView;
 
 /**
@@ -45,6 +46,57 @@ public class RoomMode extends NeuroModePane {
         ImageView cPanelView = new ImageView(getResourceManager().getSprite("NEURO_1"));
         RoomPane roomPane = new RoomPane(resourceManager, room);
         getChildren().addAll(cPanelView, roomPane);
+
+        setOnMouseClicked((t) -> {
+            handleMouseClick(t.getX(), t.getY());
+        });
     }
 
+    @Override
+    public void tick() {
+        // ???
+    }
+
+    @Override
+    public void destroy() {
+        room = null;
+    }
+
+    private void handleMouseClick(double x, double y) {
+        LOGGER.log(Level.SEVERE, "Mouse Click at: {0},{1}", new Object[]{x, y});
+        if ((y > 16 && y < 240) && (x > 16 && x < 624)) {
+            // User clicked in room scene.
+            LOGGER.log(Level.SEVERE, "User clicked in scene at: {0},{1}", new Object[]{x, y});
+        } else if (y > 292 && y < 336 && x > 32 && x < 81) {
+            // inventory
+            LOGGER.log(Level.SEVERE, "User clicked Inventory.");
+        } else if (y > 292 && y < 336 && x > 70 && x < 108) {
+            // pax
+            LOGGER.log(Level.SEVERE, "User clicked PAX.");
+        } else if (y > 292 && y < 336 && x > 109 && x < 172) {
+            // talk
+            LOGGER.log(Level.SEVERE, "User clicked Talk.");
+        } else if (y > 336 && y < 380 && x > 32 && x < 81) {
+            // skills
+            LOGGER.log(Level.SEVERE, "User clicked Skills.");
+        } else if (y > 336 && y < 380 && x > 70 && x < 108) {
+            // rom
+            LOGGER.log(Level.SEVERE, "User clicked ROM.");
+        } else if (y > 336 && y < 380 && x > 109 && x < 172) {
+            // disk
+            LOGGER.log(Level.SEVERE, "User clicked Disk.");
+        } else if (y > 334 && y < 356 && x > 224 && x < 256) {
+            // disk
+            LOGGER.log(Level.SEVERE, "User clicked Date Status.");
+        } else if (y > 334 && y < 356 && x > 256 && x < 288) {
+            // disk
+            LOGGER.log(Level.SEVERE, "User clicked Time Status.");
+        } else if (y > 356 && y < 378 && x > 224 && x < 256) {
+            // disk
+            LOGGER.log(Level.SEVERE, "User clicked Credits Status.");
+        } else if (y > 356 && y < 378 && x > 256 && x < 288) {
+            // disk
+            LOGGER.log(Level.SEVERE, "User clicked Constitution Status.");
+        }
+    }
 }
