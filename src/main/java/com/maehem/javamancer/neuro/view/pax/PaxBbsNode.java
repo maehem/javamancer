1b03787fe8ff501c06ba57bcdaa7e9ab0fa527ce
Mark J Koch <maehem@users.noreply.github.com>
2024-07-23 12:38:44 -0700

    Message send working.

--- src/main/java/com/maehem/javamancer/neuro/view/pax/PaxBbsNode.java
+++ src/main/java/com/maehem/javamancer/neuro/view/pax/PaxBbsNode.java
@@ -35,6 +35,7 @@ import java.util.logging.Level;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
 import static javafx.scene.input.KeyCode.DIGIT1;
 import static javafx.scene.input.KeyCode.DIGIT2;
 import static javafx.scene.input.KeyCode.DIGIT3;
@@ -42,7 +43,9 @@ import static javafx.scene.input.KeyCode.DIGIT4;
 import static javafx.scene.input.KeyCode.DIGIT5;
 import static javafx.scene.input.KeyCode.DOWN;
 import static javafx.scene.input.KeyCode.ESCAPE;
+import static javafx.scene.input.KeyCode.S;
 import static javafx.scene.input.KeyCode.UP;
+import static javafx.scene.input.KeyCode.V;
 import static javafx.scene.input.KeyCode.X;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
@@ -60,25 +63,75 @@ import javafx.scene.transform.Scale;
 public class PaxBbsNode extends PaxNode {
 
     private static final int NUM_MESSAGES = 5;
+    private Text toCaretText;
+    private Text msgCaretText;
 
     private enum Mode {
-        LIST, MESSAGE
+        MENU, LIST, VIEW, SEND
+    }
+
+    private enum SendMode {
+        TO, MESSAGE
     }
 
     private final ResourceManager resourceManager;
 
     private Mode mode = Mode.LIST;
+    private SendMode sendMode = SendMode.TO;
+
     private int messageIndex = 0;
     private int numMessages = 0;
+    private final StringBuilder typedTo = new StringBuilder();
+    private final StringBuilder typedMessage = new StringBuilder();
+    private final Text sendToText = new Text("");
+    private final Text sendMsgText = new Text("");
 
     public PaxBbsNode(PaxNodeListener l, GameState gs, ResourceManager rm) {
         super(l, gs);
         this.resourceManager = rm;
 
-        messageListPage();
+        modeMenu();
     }
 
-    private void messageListPage() {
+    private void modeMenu() {
+        getChildren().clear();
+        mode = Mode.MENU;
+
+        messageIndex = 0;
+
+        Text header = new Text("Bulletin Board");
+        Text exitItem = new Text("X. Exit To Main\n");
+        Text viewItem = new Text("V. View Messages\n");
+        Text sendItem = new Text("S. Send Message\n");
+        TextFlow menuItems = new TextFlow(
+                exitItem,
+                viewItem,
+                sendItem
+        );
+        menuItems.setLineSpacing(-6);
+
+        VBox box = new VBox(
+                header,
+                menuItems
+        );
+        box.getTransforms().add(new Scale(1.3, 1.0));
+        box.setSpacing(8);
+        box.setLayoutX(10);
+
+        exitItem.setOnMouseClicked((t) -> {
+            listener.paxNodeExit();
+        });
+        viewItem.setOnMouseClicked((t) -> {
+            viewpage();
+        });
+        sendItem.setOnMouseClicked((t) -> {
+            sendPage();
+        });
+
+        getChildren().add(box);
+    }
+
+    private void viewpage() {
         getChildren().clear();
         mode = Mode.LIST;
 
@@ -120,7 +173,7 @@ public class PaxBbsNode extends PaxNode {
             handleEvent(new KeyEvent(KeyEvent.KEY_PRESSED, null, null, UP, true, true, true, true));
         });
         exit.setOnMouseClicked((t) -> {
-            listener.paxNodeExit();
+            modeMenu();
         });
         more.setOnMouseClicked((t) -> {
             handleEvent(new KeyEvent(KeyEvent.KEY_PRESSED, null, null, DOWN, true, true, true, true));
@@ -136,7 +189,7 @@ public class PaxBbsNode extends PaxNode {
                 if (article.show) {
                     Text messageItem = new Text((i + 1) + ". " + article.toListString() + "\n");
                     tf.getChildren().add(messageItem);
-                    final int n = i + 1;
+                    final int n = i + messageIndex;
                     messageItem.setOnMouseClicked((t) -> {
                         showMessage(gameState.bbs.get(n));
                     });
@@ -144,12 +197,12 @@ public class PaxBbsNode extends PaxNode {
             }
         }
         numMessages = tf.getChildren().size();
-        LOGGER.log(Level.FINER, "Num Messages: {0}", numMessages);
+        LOGGER.log(Level.SEVERE, "Num Messages: {0}", numMessages);
     }
 
     private void showMessage(BbsMessage message) {
         getChildren().clear();
-        mode = Mode.MESSAGE;
+        mode = Mode.VIEW;
         Text heading = new Text(message.date);
         Text back = new Text("back");
         HBox navBox = new HBox(back);
@@ -178,11 +231,59 @@ public class PaxBbsNode extends PaxNode {
         });
     }
 
+    private void sendPage() {
+        getChildren().clear();
+        mode = Mode.SEND;
+        sendMode = SendMode.TO;
+
+        Text header = new Text("     Send Message\nPress ESC when done");
+
+        Text dateText = new Text("date:  " + gameState.getDateString() + "\n");
+        Text toText = new Text("to  :  ");
+        toCaretText = new Text("<");
+        //Text msgText = new Text("");
+        msgCaretText = new Text("");
+        Text newLineText = new Text("\n");
+
+        TextFlow tf = new TextFlow(dateText,
+                toText, sendToText, toCaretText,
+                newLineText,
+                sendMsgText, msgCaretText);
+        tf.setLineSpacing(-8);
+        VBox box = new VBox(
+                header,
+                tf
+        );
+        box.setSpacing(8);
+        box.getTransforms().add(new Scale(1.33, 1.0));
+        box.setMinWidth(518);
+        box.setPrefWidth(518);
+        box.setMinHeight(200);
+        box.setPadding(new Insets(0, 0, 0, 6));
+        VBox.setVgrow(tf, Priority.ALWAYS);
+
+        getChildren().add(box);
+
+    }
+
     @Override
     public boolean handleEvent(KeyEvent ke) {
         switch (mode) {
+            case MENU -> {
+                switch (ke.getCode()) {
+                    case X -> {
+                        listener.paxNodeExit();
+                    }
+                    case V -> {
+                        viewpage();
+                    }
+                    case S -> {
+                        sendPage();
+                    }
+                }
+            }
             case LIST -> {
-                try {
+                try { // TODO: Still need this try statment??
                     switch (ke.getCode()) {
                         case X -> {
                             listener.paxNodeExit();
@@ -206,14 +307,14 @@ public class PaxBbsNode extends PaxNode {
                             if (messageIndex >= NUM_MESSAGES) {
                                 messageIndex -= NUM_MESSAGES;
                                 LOGGER.log(Level.FINEST, "BBS Message Index: {0}/{1}", new Object[]{messageIndex, gameState.news.size()});
-                                messageListPage();
+                                viewpage();
                             }
                         }
                         case DOWN -> {
                             if (messageIndex < numAvailMessages() - NUM_MESSAGES) {
                                 messageIndex += NUM_MESSAGES;
                                 LOGGER.log(Level.FINEST, "BBS Message Index: {0}/{1}", new Object[]{messageIndex, gameState.news.size()});
-                                messageListPage();
+                                viewpage();
                             }
                         }
                     }
@@ -222,18 +323,49 @@ public class PaxBbsNode extends PaxNode {
                     LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
                 }
             }
-            case MESSAGE -> {
+            case VIEW -> {
                 // Up down keys
                 // ESC and X
                 switch (ke.getCode()) {
                     case ESCAPE, X -> {
-                        messageListPage();
+                        viewpage();
                     }
                     case UP -> {
                         // Scroll text
                     }
                     case DOWN -> {
                         // Scroll text
+                    }
+                }
+            }
+            case SEND -> {
+                switch (ke.getCode()) {
+                    case ESCAPE -> {
+                        // Send message
+                        sendMessage();
+                        modeMenu();
+                    }
+                    case ENTER, TAB -> { // Toggle send sub-mode
+                        if (sendMode.equals(SendMode.TO)) {
+                            updateSendMode(SendMode.MESSAGE);
+                        } else {
+                            updateSendMode(SendMode.TO);
+                        }
+                    }
+                    default -> {
+                        if (ke.getCode().isDigitKey()
+                                || ke.getCode().isLetterKey()
+                                || ke.getCode().equals(KeyCode.BACK_SPACE)) {
+                            // Typed value
+                            switch (sendMode) {
+                                case TO -> {
+                                    handleTypedTo(ke);
+                                }
+                                case MESSAGE -> {
+                                    handleTypedMsg(ke);
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -250,5 +382,72 @@ public class PaxBbsNode extends PaxNode {
             }
         }
         return i;
+    }
+
+    private void updateSendMode(SendMode mode) {
+        sendMode = mode;
+        switch (mode) {
+            case TO -> {
+                toCaretText.setText("<");
+                msgCaretText.setText("");
+            }
+            case MESSAGE -> {
+                toCaretText.setText("");
+                msgCaretText.setText("<");
+            }
+        }
+    }
+    private void handleTypedTo(KeyEvent ke) {
+        if (typedTo.length() < 12 && ke.getCode().isLetterKey()) {
+            LOGGER.log(Level.FINEST, "Typed: {0}", ke.getText());
+            typedTo.append(ke.getText());
+            sendToText.setText(typedTo.toString());
+
+        } else if (typedTo.length() > 0 && ke.getCode().equals(KeyCode.BACK_SPACE)) {
+            LOGGER.log(Level.FINEST, "Backspace.");
+            typedTo.delete(typedTo.length() - 1, typedTo.length());
+            sendToText.setText(typedTo.toString());
+        } else if (ke.getCode().equals(KeyCode.ENTER)) {
+            // Change to msgText entry
+
+        }
+    }
+
+    private void handleTypedMsg(KeyEvent ke) {
+        if (typedMessage.length() < 255 && ke.getCode().isLetterKey()) {
+            LOGGER.log(Level.FINEST, "Typed: {0}", ke.getText());
+            typedMessage.append(ke.getText());
+            sendMsgText.setText(typedMessage.toString());
+
+        } else if (typedMessage.length() > 0 && ke.getCode().equals(KeyCode.BACK_SPACE)) {
+            LOGGER.log(Level.FINEST, "Backspace.");
+            typedMessage.delete(typedMessage.length() - 1, typedMessage.length());
+            sendMsgText.setText(typedMessage.toString());
+        } else if (ke.getCode().equals(KeyCode.ENTER)) {
+            // Change to msgText entry
+
+        }
+    }
+
+    private void sendMessage() {
+        BbsMessage message = new BbsMessage(
+                gameState.getDateString(),
+                typedTo.toString(),
+                gameState.name,
+                typedMessage.toString(),
+                true
+        );
+
+        ArrayList<BbsMessage> bbs = gameState.bbs;
+        int index = 0;
+        for (int i = 0; i < bbs.size(); i++) {
+            if (bbs.get(i).show) {
+                index = i;
+            }
+        }
+        bbs.add(index + 1, message);
+
+        LOGGER.log(Level.SEVERE, "Message sent to: {0}", typedTo);
+        // Check if to armitage and contains BAMA id.
     }
 }
