08c87ed00b9a0e8e98cd87692b2718a3ecc09517
Mark J Koch <maehem@users.noreply.github.com>
2024-07-24 12:25:28 -0700

    New skills and some refactoring.

--- src/main/java/com/maehem/javamancer/neuro/model/skill/Skill.java
+++ src/main/java/com/maehem/javamancer/neuro/model/skill/Skill.java
@@ -26,31 +26,50 @@
  */
 package com.maehem.javamancer.neuro.model.skill;
 
+import com.maehem.javamancer.logging.Logging;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mark J Koch ( @maehem on GitHub )
  */
 public abstract class Skill {
 
+    public static final Logger LOGGER = Logging.LOGGER;
+    public final Type type;
+
     public enum Type {
-        NONE,
-        BARGAINING,
-        COPTALK,
-        WAREZ_ANALYSIS,
-        DEBUG,
-        HW_REPAIR,
-        ICE_BRAKING,
-        EVASION,
-        CRYPTOLOGY,
-        JAPANESE,
-        LOGIC,
-        PSYCHOANALYSIS,
-        PHENOMENOLOGY,
-        PHILOSOPHY,
-        SOPHISTRY,
-        ZEN,
-        MUSICIANSHIP
+        NONE("none", "..."),
+        BARGAINING("Barganing", "Use for getting better pricing."),
+        COPTALK("Cop Talk", "Freeze dirtag!!!"),
+        WAREZ_ANALYSIS("Warez Analysis", "What version of PowerPoint is this?"),
+        DEBUG("Debug", "More like whack-a-mole."),
+        HW_REPAIR("Hardware Repair", "Turn it off and then turn it back on."),
+        ICE_BREAKING("I.C.E. Breaking.", "I'm in!"),
+        EVASION("Evasion", "Slippery little bugger!"),
+        CRYPTOLOGY("Cryptology", "I know PGP!"),
+        JAPANESE("Japanese", "In Ohio, goats are a mess."),
+        LOGIC("Logic", "result = 2B | !2B"),
+        PSYCHOANALYSIS("Psychoanalysis", "Come lay on the couch..."),
+        PHENOMENOLOGY("Phenomenology", "Why are we here?"),
+        PHILOSOPHY("Philosophy", "excuse me, I was still talking."),
+        SOPHISTRY("Sophistry", "Gaslight Express! Arriving on track One."),
+        ZEN("Zen", "Mommy needs some \"quiet time\"."),
+        MUSICIANSHIP("Muscianship", "Name that tune...");
+
+        public final String itemName;
+        public final String description;
+
+        private Type(String name, String description) {
+            this.itemName = name;
+            this.description = description;
+        }
+
     };
+
+    public Skill(Type type) {
+        this.type = type;
+    }
 
     public abstract void use();
 }
