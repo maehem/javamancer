f905490130ea2dda7de6f6b30cc66c924142450e
Mark J Koch <maehem@users.noreply.github.com>
2024-07-24 10:03:10 -0700

    Inventory sub menu working.

--- src/main/java/com/maehem/javamancer/neuro/view/InventoryPopup.java
+++ src/main/java/com/maehem/javamancer/neuro/view/InventoryPopup.java
@@ -32,6 +32,10 @@ import com.maehem.javamancer.neuro.model.Item;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import javafx.geometry.Insets;
+import javafx.scene.input.KeyCode;
+import static javafx.scene.input.KeyCode.DIGIT1;
+import static javafx.scene.input.KeyCode.O;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -47,13 +51,14 @@ public class InventoryPopup extends SmallPopupPane {
     private final PopupListener listener;
 
     private enum Mode {
-        MENU, DISCARD, OPERATE
+        MENU, EFFECT
     }
 
     private static final int NUM_ITEMS = 4;
     private int itemIndex = 0;
     private int numItems = 0;
     private Mode mode = Mode.MENU;
+    private InventoryItem currentItem = null;
 
     public InventoryPopup(PopupListener l, GameState gs) {
         super(gs);
@@ -64,6 +69,8 @@ public class InventoryPopup extends SmallPopupPane {
     private void itemListPage() {
         getChildren().clear();
         mode = Mode.MENU;
+        currentItem = null;
+
         Text heading = new Text("Inventory");
         Text previous = new Text("previous");
         previous.setVisible(itemIndex > 0);
@@ -77,6 +84,7 @@ public class InventoryPopup extends SmallPopupPane {
         //exit.setTextAlignment(TextAlignment.CENTER);
         TextFlow tf = new TextFlow();
         tf.setLineSpacing(-10);
+        tf.setMinHeight(76);
         VBox box = new VBox(
                 heading,
                 tf,
@@ -95,7 +103,18 @@ public class InventoryPopup extends SmallPopupPane {
         getChildren().add(box);
 
         exit.setOnMouseClicked((t) -> {
+            LOGGER.log(Level.CONFIG, "Clicked Inventory Exit.");
             listener.popupExit();
+        });
+        more.setOnMouseClicked((t) -> {
+            LOGGER.log(Level.CONFIG, "Clicked Inventory More.");
+            itemIndex += NUM_ITEMS;
+            itemListPage();
+        });
+        previous.setOnMouseClicked((t) -> {
+            LOGGER.log(Level.CONFIG, "Clicked Inventory Previous.");
+            itemIndex -= NUM_ITEMS;
+            itemListPage();
         });
     }
 
@@ -105,48 +124,128 @@ public class InventoryPopup extends SmallPopupPane {
         for (int i = 0; i < NUM_ITEMS; i++) {
             if (i + itemIndex < articles.size()) {
                 String newLine = i > 0 ? "\n" : "";
-                InventoryItem article = articles.get(i + itemIndex);
-                Text messageItem;
-                if (article.item.equals(Item.CREDITS)) {
-                    messageItem = new Text(newLine + (i + 1) + ". " + article.item.itemName + " " + gameState.chipBalance);
+                InventoryItem item = articles.get(i + itemIndex);
+                Text listItem;
+                if (item.item.equals(Item.CREDITS)) {
+                    listItem = new Text(newLine + (i + 1) + ". " + item.item.itemName + " " + gameState.chipBalance);
                 } else {
-                    messageItem = new Text(newLine + (i + 1) + ". " + article.item.itemName);
+                    listItem = new Text(newLine + (i + 1) + ". " + item.item.itemName);
                 }
-                tf.getChildren().add(messageItem);
-                final int n = i + itemIndex;
-                messageItem.setOnMouseClicked((t) -> {
-                    //showMessage(gameState.bbs.get(n));
+
+                tf.getChildren().add(listItem);
+                final int n = i;
+                listItem.setOnMouseClicked((t) -> {
+                    effectItem(n);
                 });
             }
         }
         numItems = tf.getChildren().size();
         LOGGER.log(Level.SEVERE, "Num Messages: {0}", numItems);
+    }
 
-//        for (InventoryItem ii : gameState.inventory) {
-//            switch (ii.item.type) {
-//                case CREDITS -> {
-//                    Text itemText = new Text("\n" + itemIndex + ". " + ii.item.itemName + " " + gameState.chipBalance);
-//                    // set on mouse clicked...
-//                    tf.getChildren().add(tf);
-//                    itemIndex++;
-//                }
-//                case REAL -> {
-//                    Text itemText = new Text("\n" + itemIndex + ". " + ii.item.itemName);
-//                    // set on mouse clicked...
-//                    tf.getChildren().add(tf);
-//                }
-//                case SKILL -> {
-//                    Text itemText = new Text("\n" + itemIndex + ". " + ii.item.itemName);
-//                    // set on mouse clicked...
-//                    tf.getChildren().add(tf);
-//                }
-//                case DECK -> {
-//                    Text itemText = new Text("\n" + itemIndex + ". " + ii.item.itemName);
-//                    // set on mouse clicked...
-//                    tf.getChildren().add(tf);
-//                }
-//            }
-//        }
+    private void effectItem(int index) {
+        if (itemIndex + index < gameState.inventory.size()) {
+            currentItem = gameState.inventory.get(itemIndex + index);
 
+            getChildren().clear();
+            mode = Mode.EFFECT;
+            Text heading;
+            if (currentItem.item.equals(Item.CREDITS)) {
+                heading = new Text(currentItem.item.itemName + " " + gameState.chipBalance);
+            } else {
+                heading = new Text(currentItem.item.itemName);
+            }
+            Text exitText = new Text("X. Exit\n");
+            Text operateText = new Text("O. Operate Item\n");
+            Text discardText = new Text("D. Discard Item\n");
+            Text giveText = new Text("G. Give Item");
+
+            TextFlow tf = new TextFlow(exitText, operateText, discardText, giveText);
+            tf.setLineSpacing(-10);
+
+            VBox box = new VBox(
+                    heading,
+                    tf
+            );
+            box.setSpacing(0);
+            box.getTransforms().add(new Scale(1.33, 1.0));
+            box.setMinWidth(400);
+            box.setPrefWidth(400);
+            box.setMinHeight(160);
+            box.setMaxHeight(160);
+            box.setPadding(new Insets(0, 0, 0, 10));
+
+            getChildren().add(box);
+
+            exitText.setOnMouseClicked((t) -> {
+                itemListPage();
+            });
+            operateText.setOnMouseClicked((t) -> {
+                operateItem();
+            });
+            discardText.setOnMouseClicked((t) -> {
+                discardItem();
+            });
+            giveText.setOnMouseClicked((t) -> {
+                giveItem();
+            });
+        }
+    }
+
+    @Override
+    public boolean handleKeyEvent(KeyEvent keyEvent) {
+        KeyCode code = keyEvent.getCode();
+
+        switch (mode) {
+            case MENU -> {
+                switch (code) {
+                    case DIGIT1 -> {
+                        effectItem(0);
+                    }
+                    case DIGIT2 -> {
+                        effectItem(1);
+                    }
+                    case DIGIT3 -> {
+                        effectItem(2);
+                    }
+                    case DIGIT4 -> {
+                        effectItem(3);
+                    }
+                    case X -> {
+                        return super.handleKeyEvent(keyEvent);
+                    }
+                }
+            }
+            case EFFECT -> {
+                switch (code) {
+                    case X -> {
+                        itemListPage();
+                    }
+                    case O -> {
+                        operateItem();
+                    }
+                    case D -> {
+                        discardItem();
+                    }
+                    case G -> {
+                        giveItem();
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void operateItem() {
+        LOGGER.log(Level.CONFIG, "Operate: {0}", currentItem);
+    }
+
+    private void discardItem() {
+        LOGGER.log(Level.CONFIG, "Discard: {0}", currentItem);
+    }
+
+    private void giveItem() {
+        LOGGER.log(Level.CONFIG, "Give: {0}", currentItem);
     }
 }
