7bcda0a9a76d8c876bae5b98466adf3e3a30c983
Mark J Koch <maehem@users.noreply.github.com>
2024-08-17 09:26:03 -0700

    Clean up code.

--- src/main/java/com/maehem/javamancer/neuro/model/room/R32Extras.java
+++ src/main/java/com/maehem/javamancer/neuro/model/room/R32Extras.java
@@ -27,7 +27,6 @@
 package com.maehem.javamancer.neuro.model.room;
 
 import com.maehem.javamancer.neuro.model.GameState;
-import com.maehem.javamancer.neuro.model.Room;
 import com.maehem.javamancer.neuro.model.RoomExtras;
 import com.maehem.javamancer.neuro.model.item.Item;
 import java.util.Map;
@@ -111,7 +110,7 @@ public class R32Extras extends RoomExtras { // Metro Holographix
     public void initRoom(GameState gs) {
         // lock door if still talking to Ratz.
         //gs.doorBottomLocked = gs.roomNpcTalk[gs.room.getIndex()];
-        gs.resourceManager.getRoomText(Room.R32).dumpList();
+        //gs.resourceManager.getRoomText(Room.R32).dumpList();
     }
 
     @Override
