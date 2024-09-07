452b47a05c0629955eb3854739a6eac6ee95e143
Mark J Koch <maehem@users.noreply.github.com>
2024-09-06 22:34:25 -0700

    Add Message view/send features. Add Downloads.

--- src/main/java/com/maehem/javamancer/neuro/view/database/DatabaseView.java
+++ src/main/java/com/maehem/javamancer/neuro/view/database/DatabaseView.java
@@ -27,16 +27,23 @@
 package com.maehem.javamancer.neuro.view.database;
 
 import com.maehem.javamancer.logging.Logging;
+import com.maehem.javamancer.neuro.model.BbsMessage;
 import com.maehem.javamancer.neuro.model.GameState;
+import com.maehem.javamancer.neuro.model.TextResource;
 import com.maehem.javamancer.neuro.model.database.Database;
+import com.maehem.javamancer.neuro.model.warez.Warez;
 import com.maehem.javamancer.neuro.view.PopupListener;
 import com.maehem.javamancer.neuro.view.popup.PopupPane;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.KeyCode;
 import static javafx.scene.input.KeyCode.ESCAPE;
 import static javafx.scene.input.KeyCode.X;
@@ -53,9 +60,15 @@ import javafx.scene.transform.Scale;
 public abstract class DatabaseView {
 
     public static final Logger LOGGER = Logging.LOGGER;
+    private final Text typedToText = new Text();
+    private final Text typedMessageText = new Text();
+    private final Text toCursor = new Text("<");
+    private final Text messageCursor = new Text();
+    private final Text sendYN = new Text("\n        Send message? Y/N\n");
+    private final Text instructions = new Text("\n\nPress ESC to end.");
 
     protected enum SubMode {
-        LANDING, PASSWORD, CLEAR_WAIT, MAIN
+        LANDING, PASSWORD, CLEAR_WAIT, MAIN, MSG_LIST, MSG_SHOW, MSG_SEND
     }
 
     private enum AccessText {
@@ -66,9 +79,16 @@ public abstract class DatabaseView {
     protected static final double LINE_SPACING = -8.8;
     protected static final double TF_W = 420;
     protected static final Insets TF_PADDING = new Insets(8);
+    private static final String PADDING = "\n          ";
+
+    protected final TextResource dbTextResource;
 
     private final StringBuilder typedPassword = new StringBuilder();
-    protected final Text enteredText = new Text();
+    private final StringBuilder typedTo = new StringBuilder();
+    private final StringBuilder typedMessage = new StringBuilder();
+
+    protected final Text headingText = new Text();
+    protected final Text enteredPasswordText = new Text();
     protected final Text accessStatusText = new Text();
     protected boolean accessDenied = false;
 
@@ -82,6 +102,8 @@ public abstract class DatabaseView {
     protected int accessLevel = 0;
     protected SubMode subMode = SubMode.LANDING;
     private int clearWait = 0;
+    protected int messageIndex = 0;
+    private ArrayList<BbsMessage> visibleMessages = new ArrayList<>();
 
     public DatabaseView(GameState gs, Pane p, PopupListener l) {
         this.database = gs.database;
@@ -89,8 +111,18 @@ public abstract class DatabaseView {
         this.pane = p;
         this.listener = l;
 
+        buildVisibleMessagesList();
+
+        dbTextResource = gameState.resourceManager.getDatabaseText(gameState.database.number);
+
+        headingText.setText(centeredText(dbTextResource.get(0)) + "\n\n");
+
         setAccessText(AccessText.NONE);
     }
+
+    protected abstract void landingPage();
+
+    protected abstract void siteContent();
 
     public boolean handleKeyEvent(KeyEvent keyEvent) {
         KeyCode code = keyEvent.getCode();
@@ -108,6 +140,29 @@ public abstract class DatabaseView {
             }
             case PASSWORD -> {
                 handleEnteredPassword(keyEvent);
+            }
+            case MSG_SEND -> {
+                if (toCursor.isVisible()) {
+                    handleTypedBBSTo(keyEvent);
+                } else if (messageCursor.isVisible()) {
+                    handleTypedBBSMessage(keyEvent);
+                } else if (sendYN.isVisible()) {
+                    LOGGER.log(Level.SEVERE, "Handle Yes/No answer.");
+                    // Y  or N
+                    switch (code) {
+                        case Y -> {
+                            // Send the message.
+                            sendMessage(typedTo.toString(), typedMessage.toString());
+                            siteContent();
+                        }
+                        case N -> {
+                            // Back to menu
+                            siteContent();
+                        }
+                    }
+                } else {
+                    LOGGER.log(Level.SEVERE, "ERROR: Unknown Message Mode state!");
+                }
             }
         }
 
@@ -143,9 +198,16 @@ public abstract class DatabaseView {
         return null;
     }
 
-    protected String centeredText(String str) {
+    protected final String centeredText(String str) {
         int pad = str.length() + (CHAR_W - str.length()) / 2;
         return String.format("%" + pad + "s", str);
+    }
+
+    protected TextFlow pageHeadingTextFlow(Node... node) {
+        TextFlow tf = pageTextFlow(node);
+        tf.getChildren().addFirst(headingText);
+
+        return tf;
     }
 
     protected TextFlow pageTextFlow(Node... node) {
@@ -165,6 +227,19 @@ public abstract class DatabaseView {
         return t;
     }
 
+    protected TextFlow pageTextScrolledFlow(Node header, Node node) {
+        ScrollPane sp = new ScrollPane(node);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setMinSize(300, 230);
+        sp.setMaxSize(300, 230);
+
+        TextFlow mainTf = pageTextFlow(header, new Text("\n"), sp);
+        mainTf.setLineSpacing(LINE_SPACING);
+
+        return mainTf;
+    }
+
     private void passwordPage() {
         subMode = SubMode.PASSWORD;
         pane.getChildren().clear();
@@ -179,9 +254,8 @@ public abstract class DatabaseView {
         Text leadingText2 = new Text(leadingSpace);
         Text cursorText = new Text("<\n");
 
-        TextFlow tf = pageTextFlow(
-                leadingText1, instructionsText,
-                leadingText2, enteredText, cursorText,
+        TextFlow tf = pageTextFlow(leadingText1, instructionsText,
+                leadingText2, enteredPasswordText, cursorText,
                 new Text("\n\n"), accessStatusText, // need blank Text() or FX has rendering issue.
                 new Text("\n\n\n\n"), CONTINUE_TEXT
         );
@@ -197,7 +271,7 @@ public abstract class DatabaseView {
         if (typedPassword.length() < 12 && ke.getCode().isLetterKey()) {
             LOGGER.log(Level.FINEST, "Typed: {0}", ke.getText());
             typedPassword.append(ke.getText());
-            enteredText.setText(typedPassword.toString());
+            enteredPasswordText.setText(typedPassword.toString());
         } else if (typedPassword.length() > 0 && ke.getCode().equals(KeyCode.BACK_SPACE)) {
             LOGGER.log(Level.FINEST, "Backspace.");
             if (accessDenied) {
@@ -207,7 +281,7 @@ public abstract class DatabaseView {
             } else {
                 typedPassword.delete(typedPassword.length() - 1, typedPassword.length());
             }
-            enteredText.setText(typedPassword.toString());
+            enteredPasswordText.setText(typedPassword.toString());
         } else if (ke.getCode().equals(KeyCode.ENTER)) {
             LOGGER.log(Level.SEVERE, "entered: {0}  passwords: {1}, {2}",
                     new Object[]{
@@ -258,8 +332,278 @@ public abstract class DatabaseView {
         clearWait = 30; // 2 seconds.
     }
 
-    protected abstract void landingPage();
-    protected abstract void siteContent();
+    protected final void buildVisibleMessagesList() {
+        // clear list
+        visibleMessages.clear();
+        database.bbsMessages.forEach((t) -> {
+            if (t.show) {
+                visibleMessages.add(t);
+            }
+        });
+    }
+
+    protected void messages() {
+        LOGGER.log(Level.SEVERE, "{0}: Messages", database.name);
+        pane.getChildren().clear();
+        //ArrayList<BbsMessage> messageList = database.bbsMessages;
+        //mode = Mode.MSG;
+        TextFlow tf = pageHeadingTextFlow();
+        tf.getChildren().add(new Text("     date     to            from\n"));
+
+        visibleMessages.size();
+        for (int n = 0; n < 9; n++) {
+            int nn = n + 1;
+            try {
+                BbsMessage msg = visibleMessages.get(messageIndex + n);
+                String nStr = String.format("%2d", nn);
+
+                Text mmenuItem = new Text("\n " + nStr + ". " + msg.toListString(gameState.name));
+                tf.getChildren().add(mmenuItem);
+                mmenuItem.setOnMouseClicked((t) -> {
+                    t.consume();
+                    LOGGER.log(Level.SEVERE, "User clicks message: {0}", nn);
+                    showMessage(msg);
+                });
+            } catch (IndexOutOfBoundsException ex) {
+                break;
+            }
+        }
+
+        Text exitText = new Text("eXit");
+        exitText.setOnMouseClicked((t) -> {
+            t.consume();
+            siteContent();
+        });
+        Text prevText = new Text("    ");
+        if (messageIndex > 0) {
+            prevText.setText("prev");
+            prevText.setOnMouseClicked((t) -> {
+                LOGGER.log(Level.SEVERE, "User clicked messages prev.");
+                messageIndex -= 9;
+                t.consume();
+                messages();
+            });
+        }
+        Text nextText = new Text("    ");
+        if (visibleMessages.size() - (messageIndex + 9) > 0) {
+            nextText.setText("next");
+            nextText.setOnMouseClicked((t) -> {
+                LOGGER.log(Level.SEVERE, "User clicked messages next.");
+                messageIndex += 9;
+                t.consume();
+                messages();
+            });
+        }
+        tf.getChildren().addAll(
+                new Text("\n\n           "),
+                prevText,
+                new Text("    "),
+                exitText,
+                new Text("   "),
+                nextText);
+
+        pane.getChildren().add(tf);
+    }
+
+    protected void showMessage(BbsMessage msg) {
+        LOGGER.log(Level.SEVERE, "{0}: Show Message {1}", new Object[]{msg.prefillIndex, database.name});
+        pane.getChildren().clear();
+        subMode = SubMode.MSG_SHOW;
+        //mode = Mode.SHOW;
+        //BbsMessage message = database.bbsMessages.get(n);
+
+        TextFlow tf;
+        if (msg.prefillIndex >= 0) {
+            // Message is static from the game resources.
+            tf = pageHeadingTextFlow(new Text(dbTextResource.get(msg.prefillIndex).replace("\1", gameState.name)));
+        } else {
+            // Message is user generated.
+            tf = pageHeadingTextFlow(new Text(msg.body.replace("\1", gameState.name)));
+        }
+
+        pane.getChildren().add(tf);
+        pane.setOnMouseClicked((t) -> {
+            t.consume();
+            pane.setOnMouseClicked(null);
+            messages();
+        });
+    }
+
+    protected void downloads() {
+        LOGGER.log(Level.SEVERE, "{0}: Downloads", database.name);
+        pane.getChildren().clear();
+        //mode = Mode.DOWNLOADS;
+        TextFlow tf = pageHeadingTextFlow();
+        tf.getChildren().add(new Text(centeredText(" Software Library") + "\n"));
+        Text menuItem = new Text(PADDING + "X. Exit to main");
+        tf.getChildren().add(menuItem);
+        menuItem.setOnMouseClicked((t) -> {
+            t.consume();
+            LOGGER.log(Level.SEVERE, "User exit to main: ");
+            siteContent();
+        });
+
+        int i = 1;
+        i = addSoftware(i, database.warez1, tf);
+        i = addSoftware(i, database.warez2, tf);
+        addSoftware(i, database.warez3, tf);
+
+        pane.getChildren().add(tf);
+    }
+
+    private int addSoftware(int i, HashMap<Class< ? extends Warez>, Integer> map, TextFlow tf) {
+        for (Map.Entry<Class<? extends Warez>, Integer> m : map.entrySet()) {
+            try {
+                Warez w = m.getKey().getConstructor(int.class).newInstance(m.getValue());
+                Text menuItem = new Text(PADDING + i + ". " + w.getMenuString());
+                tf.getChildren().add(menuItem);
+                menuItem.setOnMouseClicked((t) -> {
+                    t.consume();
+                    LOGGER.log(Level.SEVERE, "User clicks download: {0}", w.getMenuString());
+                    attemptSoftwareDownload(w);
+                });
+                i++;
+            } catch (NoSuchMethodException | SecurityException
+                    | InstantiationException | IllegalAccessException
+                    | IllegalArgumentException | InvocationTargetException ex) {
+                LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+                ex.printStackTrace();
+            }
+        }
+
+        return i;
+    }
+
+    private void attemptSoftwareDownload(Warez w) {
+        LOGGER.log(Level.SEVERE, "Software Download: Attept to download: {0}", w.getSimpleName());
+
+        // TODO: Deck software compatibility check
+        // RESULT = deck.installSoftware( Warez )
+        gameState.usingDeck.softwarez.add(w);
+    }
+
+    protected void composeMessage() {
+        LOGGER.log(Level.SEVERE, "{0}: Compose BBS Message", database.name);
+        pane.getChildren().clear();
+        subMode = SubMode.MSG_SEND;
+
+        Text dateText = new Text("\nDate: " + gameState.getDateString());
+        Text fromText = new Text("\nFrom: " + gameState.name);
+        Text toLabel = new Text("\n  To: ");
+        typedToText.setText(null);
+
+        Text bodyStart = new Text("\n\n");
+        typedMessageText.setText("");
+
+        TextFlow tf = pageHeadingTextFlow(
+                dateText, fromText, toLabel, typedToText, toCursor,
+                bodyStart, typedMessageText, messageCursor,
+                instructions, sendYN
+        );
+
+        sendYN.setVisible(false);
+        toCursor.setVisible(true);
+        messageCursor.setVisible(false);
+
+        pane.getChildren().add(tf);
+        pane.setOnMouseClicked((t) -> {
+            t.consume();
+            siteContent();
+        });
+    }
+
+    protected void handleTypedBBSTo(KeyEvent ke) {
+        LOGGER.log(Level.FINEST, "Handle message 'to' typed.");
+        KeyCode code = ke.getCode();
+        if (typedTo.length() < GameState.NAME_LEN_MAX
+                && (code.isLetterKey() || code.isDigitKey())) {
+            LOGGER.log(Level.FINEST, "Typed: {0}", ke.getText());
+            typedTo.append(ke.getText());
+            typedToText.setText(typedTo.toString());
+        } else if (typedMessage.length() > 0
+                && ke.getCode().equals(KeyCode.BACK_SPACE)) {
+            LOGGER.log(Level.FINEST, "Backspace.");
+            typedTo.delete(typedTo.length() - 1, typedTo.length());
+            typedToText.setText(typedTo.toString());
+        } else if (ke.getCode().equals(KeyCode.ENTER)) { // Done with To, now Body field
+            LOGGER.log(Level.SEVERE, "Message 'to' complete ==> {0}",
+                    new Object[]{typedTo});
+            toCursor.setVisible(false);
+            messageCursor.setVisible(true);
+            messageCursor.setText("<");
+        }
+    }
+
+    protected void handleTypedBBSMessage(KeyEvent ke) {
+        LOGGER.log(Level.FINEST, "Handle message body typed.");
+        KeyCode code = ke.getCode();
+        if (typedMessage.length() < 160
+                && (code.isLetterKey() || code.isDigitKey()
+                || code.isWhitespaceKey()
+                || code.equals(KeyCode.PERIOD)
+                || code.equals(KeyCode.COMMA)
+                || code.equals(KeyCode.SEMICOLON)
+                || code.equals(KeyCode.QUOTE)
+                || code.equals(KeyCode.QUOTEDBL)
+                || code.equals(KeyCode.ENTER)
+                || code.equals(KeyCode.DOLLAR)
+                || code.equals(KeyCode.POUND)
+                || code.equals(KeyCode.EXCLAMATION_MARK)
+                || code.equals(KeyCode.AMPERSAND)
+                || code.equals(KeyCode.ASTERISK)
+                || code.equals(KeyCode.OPEN_BRACKET)
+                || code.equals(KeyCode.CLOSE_BRACKET)
+                || code.equals(KeyCode.SLASH)
+                || code.equals(KeyCode.BRACELEFT)
+                || code.equals(KeyCode.BRACERIGHT)
+                || code.equals(KeyCode.MINUS)
+                || code.equals(KeyCode.EQUALS)
+                || code.equals(KeyCode.PLUS)
+                || code.equals(KeyCode.UNDERSCORE)
+                || code.equals(KeyCode.RIGHT_PARENTHESIS)
+                || code.equals(KeyCode.LEFT_PARENTHESIS)
+                || code.equals(KeyCode.CIRCUMFLEX)
+                || "~".equals(ke.getCharacter())
+                || "%".equals(ke.getCharacter())
+                || code.equals(KeyCode.ENTER))) {
+            LOGGER.log(Level.FINEST, "Typed: {0}", ke.getText());
+            typedMessage.append(ke.getText());
+            typedMessageText.setText(typedMessage.toString());
+        } else if (typedMessage.length() > 0
+                && ke.getCode().equals(KeyCode.BACK_SPACE)) {
+            LOGGER.log(Level.FINEST, "Backspace.");
+            typedMessage.delete(typedMessage.length() - 1, typedMessage.length());
+            typedMessageText.setText(typedMessage.toString());
+        } else if (ke.getCode().equals(KeyCode.ESCAPE)) { // Send message
+            LOGGER.log(Level.SEVERE, "Ready to send message.");
+            instructions.setVisible(false);
+            messageCursor.setVisible(false);
+            sendYN.setVisible(true);
+            pane.requestLayout();
+        }
+        ke.consume();
+    }
+
+    /**
+     *
+     * @param to
+     * @param message
+     */
+    protected BbsMessage sendMessage(String to, String message) {
+        LOGGER.log(Level.SEVERE, "Send Message:");
+        String refinedMessage = "To: " + to + "\nFrom: \1\n" + message;
+        BbsMessage msg = new BbsMessage(database.number, gameState.getDateString(), to, "\1", refinedMessage, true);
+        database.bbsMessages.add(msg);
+        gameState.messageSent.add(msg);
+        buildVisibleMessagesList();
+
+        return msg;
+    }
+
+    protected void enableMessage(BbsMessage item) {
+        database.enableMessage(item);
+        buildVisibleMessagesList();
+    }
 
     public void tick() {
         if (subMode.equals(SubMode.CLEAR_WAIT)) {
