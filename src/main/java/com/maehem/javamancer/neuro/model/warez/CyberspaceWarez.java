fbf31c36afe441a1dc32fbcf6af0774f3284d8f1
Mark J Koch <maehem@users.noreply.github.com>
2024-09-06 22:37:23 -0700

    Create CyberspaceWarez.java

---
+++ src/main/java/com/maehem/javamancer/neuro/model/warez/CyberspaceWarez.java
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.neuro.model.warez;
+
+import com.maehem.javamancer.neuro.model.GameState;
+import com.maehem.javamancer.neuro.model.item.Item;
+
+/**
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class CyberspaceWarez extends LinkWarez {
+
+    public static final String USE_NO_JACK = "No jack found.";
+
+    public CyberspaceWarez(int version) {
+        super(Item.Catalog.CYBERSPACE, version);
+    }
+
+    @Override
+    public String use(GameState gs) {
+        String superUse = super.use(gs);
+        if (!superUse.equals(Warez.USE_OK)) {
+            return superUse;
+        }
+
+        if (gs.room.extras != null) {
+            // Must be jack in room.
+            int jackZone = gs.room.extras.jackZone();
+            if (jackZone < 0) {
+                // Fail.
+                return USE_NO_JACK;
+            }
+
+            return Warez.USE_OK;
+        }
+        return USE_NO_JACK;
+    }
+
+}
