8363d1110738a7e2d98970f0082546d9ae51b6c2
Mark J Koch <maehem@users.noreply.github.com>
2024-09-08 15:26:42 -0700

    New Fake Warez.

---
+++ src/main/java/com/maehem/javamancer/neuro/model/warez/FakeWarez.java
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.neuro.model.warez;
+
+import com.maehem.javamancer.neuro.model.GameState;
+import com.maehem.javamancer.neuro.model.item.Item;
+
+/**
+ * Fake Warez -- causes player damage
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public abstract class FakeWarez extends Warez {
+
+    public FakeWarez(Item.Catalog catItem, int version) {
+        super(catItem, version);
+    }
+
+    @Override
+    public String use(GameState gs) {
+        if (gs.usingDeck != null) {
+            switch (gs.usingDeck.getMode()) {
+                case NONE, LINKCODE -> {
+                    // Can't be used here.
+                    return "Can't be used here.";
+                }
+                case CYBERSPACE -> {
+                    // TODO: Check if at DB and DB has Ai.
+
+                    return USE_OK;
+                }
+            }
+        }
+        return "Can't be used here.";
+    }
+}
