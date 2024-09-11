0c26a70d11bcc35330b426c99b251b07e92d9db3
Mark J Koch <maehem@users.noreply.github.com>
2024-09-10 20:51:52 -0700

    New Warez

---
+++ src/main/java/com/maehem/javamancer/neuro/model/warez/BudgetPalWarez.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.neuro.model.warez;
+
+import com.maehem.javamancer.neuro.model.GameState;
+import com.maehem.javamancer.neuro.model.item.Item;
+
+/**
+ * Completely random ICE damage. Mostly harmless but 1% chance of 1000 damage.
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class BudgetPalWarez extends IceBreakerWarez {
+
+    public BudgetPalWarez(int version) {
+        super(Item.Catalog.BUDGETPAL, version);
+    }
+
+    @Override
+    public int getRunDuration() {
+        return (int) (Math.random() * 10000 + 200);
+    }
+
+    @Override
+    public int getEffect(GameState gameState) {
+        int damage = (int) (Math.random() * 1000 + 200);
+        return Math.random() < 0.01 ? 1000 : damage; // 1% chance of 1000 crit hit.
+    }
+
+}
