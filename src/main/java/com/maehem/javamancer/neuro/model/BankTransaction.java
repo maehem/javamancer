c549c96e56cf53e93800cc48d4253a1fab4c0b34
Mark J Koch <maehem@users.noreply.github.com>
2024-10-10 19:24:44 -0700

    Add features for save/load operations.

--- src/main/java/com/maehem/javamancer/neuro/model/BankTransaction.java
+++ src/main/java/com/maehem/javamancer/neuro/model/BankTransaction.java
@@ -26,11 +26,17 @@
  */
 package com.maehem.javamancer.neuro.model;
 
+import com.maehem.javamancer.logging.Logging;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mark J Koch ( @maehem on GitHub )
  */
 public class BankTransaction {
+    public static final Logger LOGGER = Logging.LOGGER;
 
     public enum Operation {
         Download, Upload, Fine, TransferIn, TransferOut
@@ -51,5 +57,34 @@ public class BankTransaction {
         return date + " " + String.format("%-11s", op.name()) + " " + String.format("%1$10s", amount);
     }
 
+    /**
+     *
+     * @param prefix
+     * @param p
+     */
+    public void putProps(String prefix, Properties p) {
+        p.put(prefix + ".date", date);
+        p.put(prefix + ".op", op.name());
+        p.put(prefix + ".amount", String.valueOf(amount));
+    }
+
+    public static BankTransaction pullTransaction(String prefix, Properties p) {
+        String date = p.getProperty(prefix + ".date", "ERROR");
+        String op = p.getProperty(prefix + ".op", "ERROR");
+        int amount = Integer.parseInt(p.getProperty(prefix + ".amount", "ERROR"));
+        LOGGER.log(Level.SEVERE, "Restore Bank Transaction: {0} :: {1}", new Object[]{date, op});
+
+        return new BankTransaction(date, lookup(op), amount);
+    }
+
+    private static Operation lookup(String opName) {
+        for (Operation op : Operation.values()) {
+            if (opName.equals(op.name())) {
+                return op;
+            }
+        }
+
+        return null;
+    }
 
 }
