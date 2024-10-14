86efabac4e22041c4478730141009765a4a7d4f1
Mark J Koch <maehem@users.noreply.github.com>
2024-10-14 13:26:30 -0700

    Tune dialog popup size.

--- src/main/java/com/maehem/javamancer/neuro/view/popup/DialogPopupPane.java
+++ src/main/java/com/maehem/javamancer/neuro/view/popup/DialogPopupPane.java
@@ -36,9 +36,9 @@ import com.maehem.javamancer.neuro.view.PopupListener;
 public abstract class DialogPopupPane extends PopupPane {
 
     private static final int WIDTH = (int) (624);
-    private static final int HEIGHT = 80;
+    private static final int HEIGHT = 70;
     private static final int X = 8;
-    private static final int Y = 8;
+    private static final int Y = 2;
 
     public DialogPopupPane(PopupListener l, GameState gs) {
         this(l, gs, WIDTH);
