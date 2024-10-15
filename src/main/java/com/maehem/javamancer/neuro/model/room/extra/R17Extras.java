ec229778ed4e97430de0663a0cc7424b25299a7e
Mark J Koch <maehem@users.noreply.github.com>
2024-10-15 13:11:07 -0700

    Extras for room 17 Street Maas Bio.

---
+++ src/main/java/com/maehem/javamancer/neuro/model/room/extra/R17Extras.java
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Mark J. Koch ( @maehem on GitHub )
+ *
+ * Portions of this software are Copyright (c) 2018 Henadzi Matuts and are
+ * derived from their project: https://github.com/HenadziMatuts/Reuromancer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.maehem.javamancer.neuro.model.room.extra;
+
+import com.maehem.javamancer.neuro.model.GameState;
+import static com.maehem.javamancer.neuro.model.room.DialogCommand.LONG_DESC;
+import static com.maehem.javamancer.neuro.model.room.DialogCommand.SHORT_DESC;
+import com.maehem.javamancer.neuro.model.room.RoomExtras;
+
+/**
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class R17Extras extends RoomExtras { // Street Maas Biolabs
+
+    protected static final int[][] DIALOG_CHAIN = {
+        {LONG_DESC.num}, {SHORT_DESC.num} //  [0] ::
+    };
+
+    @Override
+    public void initRoom(GameState gs) {
+    }
+
+    @Override
+    public int[][] getDialogChain() {
+        return null;
+    }
+
+    @Override
+    public int dialogWarmUp(GameState gs) {
+//        if (gs.room.isDoorLocked(RoomBounds.Door.LEFT)) {
+//            return 1;
+//        }
+//        return -1;
+        return 0;
+    }
+
+}
