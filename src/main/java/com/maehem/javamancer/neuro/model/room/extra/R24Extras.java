4b411b4213e716fd86d6dbf749d889aea17bf55d
Mark J Koch <maehem@users.noreply.github.com>
2025-12-16 10:15:24 -0800

    Tune dialog actions.

--- src/main/java/com/maehem/javamancer/neuro/model/room/extra/R24Extras.java
+++ src/main/java/com/maehem/javamancer/neuro/model/room/extra/R24Extras.java
@@ -29,14 +29,14 @@ package com.maehem.javamancer.neuro.model.room.extra;
 import com.maehem.javamancer.neuro.model.GameState;
 import com.maehem.javamancer.neuro.model.room.DialogCommand;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.CHIP;
-import static com.maehem.javamancer.neuro.model.room.DialogCommand.DIALOG_END;
+import static com.maehem.javamancer.neuro.model.room.DialogCommand.DESC;
+import static com.maehem.javamancer.neuro.model.room.DialogCommand.DIALOG_CLOSE;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.EXIT_L;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.INFO_BUY;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.LONG_DESC;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.MASSAGE_BOT;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.NPC;
 import static com.maehem.javamancer.neuro.model.room.DialogCommand.SHORT_DESC;
-import static com.maehem.javamancer.neuro.model.room.DialogCommand.TO_JAIL;
 import com.maehem.javamancer.neuro.model.room.RoomExtras;
 import java.util.logging.Level;
 
@@ -54,21 +54,22 @@ public class R24Extras extends RoomExtras {
         {8}, // [5] :: Uh, excuse me, Im just
         {19, CHIP.num, 20}, // [6] :: I wanna buy some info
         {}, // [7] ::
-        {DIALOG_END.num}, // [8] :: You dont know what youre
+        {DIALOG_CLOSE.num}, // [8] :: You dont know what youre
         {EXIT_L.num}, // [9] :: I just remembered I have
         {NPC.num, 21}, // [10] :: Heres a hot tip. The Panther
         {NPC.num, 21}, // [11] :: The banking center is on the
         {NPC.num, 21}, // [12] :: Be careful when dealing with the
         {NPC.num, 21}, // [13] :: Some cowboy said he avoids court
-        {NPC.num, 21}, // [14] :: Just heard that Maas Biolabs finished
+        {NPC.num, 15}, // [14] :: Just heard that Maas Biolabs finished
         {NPC.num, 21}, // [15] :: CyberEyes, you dont need a regular
         {EXIT_L.num}, // [16] :: Take a hike, meatball!
-        {TO_JAIL.num}, // [17] :: Look out!  Its
+        {}, // 17 test
+        //{TO_JAIL.num}, // [17] :: Look out!  Its
         {EXIT_L.num}, // [18] :: Get serious!  No discounts here, buckaroo!
         {INFO_BUY.num, 20}, // [19] :: You just bought yourself some
-        {}, // [20] :: The girl relieves you of <== Place in room description
+        {DESC.num}, // [20] :: The girl relieves you of <== Place in room description
         {MASSAGE_BOT.num, NPC.num,17}, // [21] :: I have more info, if you
-        {} // [22] :: You know more than me
+        {DIALOG_CLOSE.num} // [22] :: You know more than me
     };
     
     // Animation
@@ -96,6 +97,7 @@ public class R24Extras extends RoomExtras {
     public void initRoom(GameState gs) {
         // lock door if still talking to Ratz.
         //gs.doorBottomLocked = gs.roomNpcTalk[gs.room.getIndex()];
+        //gs.resourceManager.getRoomText(Room.R24).dumpList();
     }
 
     @Override
@@ -112,16 +114,18 @@ public class R24Extras extends RoomExtras {
     public int onInfoBuy(GameState gs) {
         LOGGER.log(Level.INFO, "Massage Parlor: Player bought info...");
         
-        // TODO: Make police bot appear.
-        
-        
         if (gs.chipBalance < 20) {
-            return 21;
+            return 18;
         }
 
-        gs.chipBalance -= 20;
-
-        return 10 + (int) (Math.random() * 6);
+        gs.chipBalance -= 20; // Deduct some money.
+        int randomInfo = 10 + (int) (Math.random() * 6);
+        LOGGER.log(Level.CONFIG, () -> "Akiko gives you info dialog " + randomInfo);
+        
+        // TODO: Track what random info was given and don't give it again.
+        // TODO: When all info has been bought, return 22.
+        
+        return randomInfo;
     }
 
     @Override
@@ -136,6 +140,5 @@ public class R24Extras extends RoomExtras {
         
         return 0;
     }
-
     
 }
