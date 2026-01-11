afa00005e8d5ac8d199c2141f6f5f47b62e66f2e
Mark J Koch <maehem@users.noreply.github.com>
2026-01-11 08:16:53 -0800

    Add base constitution to constructor. Add notes. Add applyWarezAttack().

--- src/main/java/com/maehem/javamancer/neuro/model/ai/AI.java
+++ src/main/java/com/maehem/javamancer/neuro/model/ai/AI.java
@@ -27,6 +27,10 @@
 package com.maehem.javamancer.neuro.model.ai;
 
 import com.maehem.javamancer.logging.Logging;
+import com.maehem.javamancer.neuro.model.GameState;
+import com.maehem.javamancer.neuro.model.item.Item;
+import com.maehem.javamancer.neuro.model.skill.Skill;
+import com.maehem.javamancer.neuro.model.warez.Warez;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
@@ -34,19 +38,45 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * <pre>
+ * There are specific anti-AI software and skills. Each AI is weak to one
+ * particular software or skill and hitting them with their weakness will either
+ * kill them outright or open them up to further attacks.
  *
+ *      Psychoanalysis skill will occasionally reveal whether an AI is weak
+ *      against a particular skill.
+ *      Software weaknesses are revealed through text messages in databases.
+ * 
+ * The four anti-AI @Skill(s) are: Philosophy, Sophistry, Phenomenology and Logic.
+ * The three anti-AI @Warez are BattleChess, Hemlock and KuangEleven. 
+ *      When you use an AI’s weakness against it, it will insult you. 
+ *      If it survives being struck by its weakness, player can then use any of the 
+ *      other anti-AI skills against it. Like with ICE battles, you should cycle 
+ *      through the skills but more for the purpose of levelling up your skills 
+ *      than maximising damage.
+ *</pre>
+ * 
  * @author Mark J Koch ( @maehem on GitHub )
  */
-public class AI {
+public abstract class AI {
+
     public static final Logger LOGGER = Logging.LOGGER;
 
     public final String name;
-    public final int index; // matching the sprite face
-    private int constitution = 2000;
+    public final int index; // matching the sprite face graphic
+    private int constitution;
+    public final int MAX_CONSTITUTION;
 
-    public AI(String name, int index) {
+    public AI(String name, int index, int constitution) {
         this.name = name;
         this.index = index;
+        this.MAX_CONSTITUTION = constitution;
+        this.constitution = constitution;
+        // For future reference:
+        // constitution formula :: (0x30 << index)
+        // except Greystoke and Neuromancer:
+        //     Greystoke :: (0x30 << index) - 1
+        //   Neuromancer :: ( 0x30 << (index-1) )
     }
 
     public void setConstitution(int value) {
@@ -57,12 +87,29 @@ public class AI {
         return constitution;
     }
 
-    public void applyDamage(int value) {
-        this.constitution -= value;
+    public void applyWarezAttack(Warez warez, GameState gs) {
+        int effect = warez.getEffect(gs);
+        this.constitution -= effect;
         if (constitution < 0) {
             constitution = 0;
         }
+        LOGGER.log(Level.INFO, "AI Takes Damage from {0} Warez of {1}.  AI constitution: {2}", new Object[]{warez.item.name(), effect, constitution});
     }
+    
+    public void applySkillAttack(Skill skill, GameState gs ) {
+        int effect = skill.getEffect(gs);
+        this.constitution -= effect;
+        if (constitution < 0) {
+            constitution = 0;
+        }
+        LOGGER.log(Level.INFO, "AI Takes Damage from {0} Skill of {1}.  AI constitution: {2}", new Object[]{skill.catalog.name(), effect, constitution});
+    }
+
+    public int getEffect() {
+        return MAX_CONSTITUTION / 10;
+    }
+
+    public abstract Item.Catalog getWeakness();
 
     /**
      *
@@ -71,13 +118,14 @@ public class AI {
      */
     public void putProps(String prefix, Properties p) {
         p.put(prefix, getClass().getSimpleName());
-        p.put(prefix + ".constitution", String.valueOf(constitution));
+        //p.put(prefix + ".constitution", String.valueOf(constitution));
     }
 
     public void pullProps(String prefix, Properties p) {
         String get = p.getProperty(prefix + ".constitution", "1");
-        LOGGER.log(Level.INFO, () -> "Restore Skill level value = " + get);
-        constitution = Integer.parseInt(get);
+        LOGGER.log(Level.INFO, () -> "Restore constitution value = " + get);
+        constitution = 0; // If it was stored in save game, it's a dead AI.
+        //constitution = Integer.parseInt(get);
     }
 
     public static AI lookup(String clazzName) {
@@ -107,7 +155,6 @@ public class AI {
                 | NoSuchMethodException
                 | SecurityException ex) {
             LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-            ex.printStackTrace();
         }
 
         return null;
