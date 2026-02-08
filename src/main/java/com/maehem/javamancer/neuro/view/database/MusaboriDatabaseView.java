8769b89e824b6f83b96835191baacc27609656bb
Mark Koch <maehem@users.noreply.github.com>
2026-02-08 09:27:06 -0800

    Tune DB appearance.

--- src/main/java/com/maehem/javamancer/neuro/view/database/MusaboriDatabaseView.java
+++ src/main/java/com/maehem/javamancer/neuro/view/database/MusaboriDatabaseView.java
@@ -74,9 +74,9 @@ public class MusaboriDatabaseView extends DatabaseView {
         pane.getChildren().clear();
         mode = Mode.SUB;
 
-        Text helloText = new Text(dbTextResource.get(3) + "\n\n\n\n");
+        Text paddingText = new Text("\n\n" + dbTextResource.get(3) + "\n\n\n\n\n\n\n");
 
-        TextFlow tf = pageTextFlow(headingText, helloText, CONTINUE_TEXT);
+        TextFlow tf = pageTextFlow(headingText, paddingText, CONTINUE_TEXT);
         pane.getChildren().add(tf);
     }
 
